6948053deef85f77c0b892bdbcf8ecaaf1f6ad1f
eghobamien vincent <uveghobamien@gmail.com>
2026-02-06 19:36:20 +0000

    Add global error handling middleware and update global usings
    Introduced ErrorHandlingMiddleware for consistent JSON error responses and exception logging across the app. Updated GlobalUsing.cs to include common namespaces for simplified codebase imports.

---
+++ MeetingIntelli/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using System.Net;
+global using System.Text.Json;
+global using  Microsoft.EntityFrameworkCore;
+global using  MeetingIntelli.Data;
+global using MeetingIntelli.Models;
+global using MeetingIntelli.Services.Interface;
