1db8b6100aa1b7d44ca985a1ed847b682bd79fb3
eghobamien vincent <uveghobamien@gmail.com>
2026-02-07 20:30:57 +0000

    Meeting methods interface

---
+++ MeetingIntelli/Contracts/IMeetings.cs
@@ -0,0 +1,29 @@
+﻿using MeetingIntelli.DTO.Requests;
+
+namespace MeetingIntelli.Contracts;
+
+public interface IMeetings
+{
+    Task<IResult> GetAllMeetings(CancellationToken ct);
+
+    Task<IResult> GetMeetingById(Guid id, CancellationToken ct);
+
+    Task<IResult> CreateMeeting(
+        CreateMeetingRequest request,
+        IMeetingAnalysisService analysisService,
+        CancellationToken ct);
+
+    Task<IResult> UpdateMeeting(
+        Guid id,
+        UpdateMeetingRequest request,
+        IMeetingAnalysisService analysisService,
+        CancellationToken ct);
+
+    Task<IResult> DeleteMeeting(Guid id, CancellationToken ct);
+
+    Task<IResult> GetStatistics(CancellationToken ct);
+
+    Task<IResult> GenerateListPdf(IPdfService pdfService, CancellationToken ct);
+
+    Task<IResult> GenerateMeetingPdf(Guid id, IPdfService pdfService, CancellationToken ct);
+}
