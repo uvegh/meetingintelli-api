7f893682ec7bea6a76dab57d2e1e2d36a6a3f7d2
eghobamien vincent <uveghobamien@gmail.com>
2026-02-10 23:50:48 +0000

    action item item table and field migration

---
+++ MeetingIntelli/Migrations/20260209231027_actionItemfield.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetingIntelli.Migrations
+{
+    /// <inheritdoc />
+    public partial class actionItemfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
