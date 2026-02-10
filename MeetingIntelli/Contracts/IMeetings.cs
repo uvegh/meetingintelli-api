61092b38bc27a017c49f5d3b6f0c1c647e71c850
eghobamien vincent <uveghobamien@gmail.com>
2026-02-10 23:52:08 +0000

    update Imeeting and set dynamic viewport for pdf

--- MeetingIntelli/Contracts/IMeetings.cs
+++ MeetingIntelli/Contracts/IMeetings.cs
@@ -6,13 +6,10 @@ public interface IMeetings
 {
     Task<IResult> GetAllMeetings(CancellationToken cancellationToken);
     Task<IResult> GetMeetingById(Guid id, CancellationToken cancellationToken);
-            Task<IResult> CreateMeetingAsync(CreateMeetingRequest request, CancellationToken cancellationToken);
-    Task<IResult> UpdateMeeting(Guid id, UpdateMeetingRequest request, 
-        CancellationToken cancellationToken);
+    Task<IResult> CreateMeetingAsync(CreateMeetingRequest request, CancellationToken cancellationToken);
+    Task<IResult> UpdateMeeting(Guid id, UpdateMeetingRequest request, CancellationToken cancellationToken);
     Task<IResult> DeleteMeeting(Guid id, CancellationToken cancellationToken);
     Task<IResult> GetStatistics(CancellationToken cancellationToken);
-    Task<IResult> GenerateListPdf(IPdfService pdfService, 
-        CancellationToken cancellationToken);
-    Task<IResult> GenerateMeetingPdf(Guid id, IPdfService pdfService,
-        CancellationToken cancellationToken);
+    Task<IResult> GenerateListPdf(IPdfService pdfService, int? viewportWidth, int? viewportHeight, CancellationToken cancellationToken);
+    Task<IResult> GenerateMeetingPdf(Guid id, IPdfService pdfService, int? viewportWidth, int? viewportHeight, CancellationToken cancellationToken);
 }
---
+++ MeetingIntelli/DTO/Requests/PdfGenerationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MeetingIntelli.DTO.Requests;
+
+public class PdfGenerationRequest
+{
+    public int ViewportWidth { get; set; } 
+    public int ViewportHeight { get; set; } 
+}
